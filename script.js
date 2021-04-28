8cdd5a76f0eb460e26bbca9578c1b595e445252d
Samuel-Trujillo <samuel.trujillo093@gmail.com>
2021-04-28 14:07:41 -0700

    alert

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var arrc = ["23.9","18.3","26.7","18.9","20.6","16.1","25.6","21.1"]
+var arrf= ["75","65","80","66","69","61","78","70"]
+
+
+
+
 var byebox=document.querySelector("#byebox")
 function removebox(){
     byebox.remove()
