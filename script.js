48d519c64c0f8781f86f3d495e8c35f0715a4419
Samuel-Trujillo <samuel.trujillo093@gmail.com>
2021-04-30 11:50:56 -0700

    FINALLY

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
 var arrc = ["23.9","18.3","26.7","18.9","20.6","16.1","25.6","21.1"]
 var arrf= ["75","65","80","66","69","61","78","70"]
 
-var high=document.querySelector(".high")
-var low=document.querySelector(".low")
-
-function changetype(){
-    high.innerHTML= (high - 32) * 5 / 9;
-    low.innerHTML= (low -32) * 5/9;
+var x=document.querySelectorAll(".high, .low")
+function changedeg(type){
+    for(let i=0; i<x.length;i++){
+        if(type=="C"){
+            x[i].innerHTML=arrc[i]
+        }
+        else{x[i].innerHTML=arrf[i]
+        }
+    }   
 }
 
-// function cToF(celsius) 
-// {
-//   var cTemp = celsius;
-//   var cToFahr = cTemp * 9 / 5 + 32;
-// }
-
-// function fToC(fahrenheit) 
-// {
-//   var fTemp = fahrenheit;
-//   var fToCel = (fTemp - 32) * 5 / 9;
-
-// } 
-// cToF(60);
-// fToC(45);
 
 
 var byebox=document.querySelector("#byebox")
